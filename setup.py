a521cd02c3e61d19cfb19acf0602e9f1811a3f58
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2022-04-12 13:36:36 +0200

    First version of  that can be installed with data convenience-functions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gazeprf',
+    version='0.0.1',
+    url='https://github.com/mszinte/gaze_prf',
+    author='Gilles de Hollander',
+    author_email='gilles.de.hollander@gmail.com',
+    packages=find_packages(),    
+    install_requires=[],
+)
