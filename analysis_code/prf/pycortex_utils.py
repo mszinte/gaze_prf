ca570c034ded438215c32b6b74d1e9d487be9e84
mszinte <mail@martinszinte.net>
2022-04-22 22:07:39 +0200

    plot brain code working

--- analysis_code/prf/pycortex_utils.py
+++ analysis_code/prf/pycortex_utils.py
@@ -9,7 +9,7 @@ def draw_cortex(subject, xfmname, data, vmin, vmax, description, cortex_type='Vo
                 alpha=None, depth=1, thick=1, height=1024, sampler='nearest',
                 with_curvature=True, with_labels=False, with_colorbar=False,
                 with_borders=False, curv_brightness=0.95, curv_contrast=0.05, add_roi=False,
-                roi_name='empty', col_offset=0, cbar_label=''):
+                roi_name='empty', col_offset=0, cbar_label='', save_fig=True):
     """
     Plot brain data onto a previously saved flatmap.
     Parameters
@@ -39,6 +39,7 @@ def draw_cortex(subject, xfmname, data, vmin, vmax, description, cortex_type='Vo
     add_roi             : add roi -image- to overlay.svg
     roi_name            : roi name
     col_offset          : colormap offset between 0 and 1
+    save_fig            : return figure
 
     Returns
     -------
@@ -67,76 +68,79 @@ def draw_cortex(subject, xfmname, data, vmin, vmax, description, cortex_type='Vo
         braindata = cortex.Volume2D(dim1=data.T, dim2=alpha.T, subject=subject, xfmname=xfmname, description=description,
                                     cmap=cmap, vmin=vmin[0], vmax=vmax[0], vmin2=vmin[1], vmax2=vmax[1])
     
-    braindata_fig = cortex.quickshow(braindata=braindata, depth=depth, thick=thick, height=height, sampler=sampler, with_curvature=with_curvature, with_labels=with_labels, 
+
+    if save_fig == True:
+        braindata_fig = cortex.quickshow(braindata=braindata, depth=depth, thick=thick, height=height, sampler=sampler, with_curvature=with_curvature, with_labels=with_labels, 
                                      with_colorbar=with_colorbar, with_borders=with_borders, curvature_brightness=curv_brightness, curvature_contrast=curv_contrast)
+        if cbar == 'polar':
+            try: base = plt.cm.get_cmap(cmap)
+            except: base = cortex.utils.get_cmap(cmap)
+            val = np.arange(1,cmap_steps+1)/cmap_steps - (1/(cmap_steps*2))
+            val = np.fmod(val+col_offset,1)
+            colmap = colors.LinearSegmentedColormap.from_list('my_colmap', base(val), N=cmap_steps)
+            cbar_axis = braindata_fig.add_axes([0.5, 0.07, 0.8, 0.2], projection='polar')
+            norm = colors.Normalize(0, 2*np.pi)
+            t = np.linspace(0,2*np.pi,200,endpoint=True)
+            r = [0,1]
+            rg, tg = np.meshgrid(r,t)
+            im = cbar_axis.pcolormesh(t, r, tg.T,norm=norm, cmap=colmap)
+            cbar_axis.set_yticklabels([])
+            cbar_axis.set_xticklabels([])
+            cbar_axis.set_theta_zero_location("W")
+            cbar_axis.spines['polar'].set_visible(False)
 
-    if cbar == 'polar':
-        try: base = plt.cm.get_cmap(cmap)
-        except: base = cortex.utils.get_cmap(cmap)
-        val = np.arange(1,cmap_steps+1)/cmap_steps - (1/(cmap_steps*2))
-        val = np.fmod(val+col_offset,1)
-        colmap = colors.LinearSegmentedColormap.from_list('my_colmap', base(val), N=cmap_steps)
-        cbar_axis = braindata_fig.add_axes([0.5, 0.07, 0.8, 0.2], projection='polar')
-        norm = colors.Normalize(0, 2*np.pi)
-        t = np.linspace(0,2*np.pi,200,endpoint=True)
-        r = [0,1]
-        rg, tg = np.meshgrid(r,t)
-        im = cbar_axis.pcolormesh(t, r, tg.T,norm=norm, cmap=colmap)
-        cbar_axis.set_yticklabels([])
-        cbar_axis.set_xticklabels([])
-        cbar_axis.set_theta_zero_location("W")
-        cbar_axis.spines['polar'].set_visible(False)
+        elif cbar == 'ecc':
+            colorbar_location = [0.5, 0.07, 0.8, 0.2]
+            n = 200
+            cbar_axis = braindata_fig.add_axes(colorbar_location, projection='polar')
+            t = np.linspace(0,2*np.pi, n)
+            r = np.linspace(0,1, n)
+            rg, tg = np.meshgrid(r,t)
+            c = tg
+            im = cbar_axis.pcolormesh(t, r, c, norm = mpl.colors.Normalize(0, 2*np.pi), cmap=colmap)
+            cbar_axis.tick_params(pad=1,labelsize=15)
+            cbar_axis.spines['polar'].set_visible(False)
+            box = cbar_axis.get_position()
+            cbar_axis.set_yticklabels([])
+            cbar_axis.set_xticklabels([])
+            axl = braindata_fig.add_axes([0.97*box.xmin,0.5*(box.ymin+box.ymax), box.width/600,box.height*0.5])
+            axl.spines['top'].set_visible(False)
+            axl.spines['right'].set_visible(False)
+            axl.spines['bottom'].set_visible(False)
+            axl.yaxis.set_ticks_position('right')
+            axl.xaxis.set_ticks_position('none')
+            axl.set_xticklabels([])
+            axl.set_yticklabels(np.linspace(vmin, vmax, 3),size = 'x-large')
+            axl.set_ylabel('$dva$\t\t', rotation=0, size='x-large')
+            axl.yaxis.set_label_coords(box.xmax+30,0.4)
+            axl.patch.set_alpha(0.5)
 
-    elif cbar == 'ecc':
-        colorbar_location = [0.5, 0.07, 0.8, 0.2]
-        n = 200
-        cbar_axis = braindata_fig.add_axes(colorbar_location, projection='polar')
-        t = np.linspace(0,2*np.pi, n)
-        r = np.linspace(0,1, n)
-        rg, tg = np.meshgrid(r,t)
-        c = tg
-        im = cbar_axis.pcolormesh(t, r, c, norm = mpl.colors.Normalize(0, 2*np.pi), cmap=colmap)
-        cbar_axis.tick_params(pad=1,labelsize=15)
-        cbar_axis.spines['polar'].set_visible(False)
-        box = cbar_axis.get_position()
-        cbar_axis.set_yticklabels([])
-        cbar_axis.set_xticklabels([])
-        axl = braindata_fig.add_axes([0.97*box.xmin,0.5*(box.ymin+box.ymax), box.width/600,box.height*0.5])
-        axl.spines['top'].set_visible(False)
-        axl.spines['right'].set_visible(False)
-        axl.spines['bottom'].set_visible(False)
-        axl.yaxis.set_ticks_position('right')
-        axl.xaxis.set_ticks_position('none')
-        axl.set_xticklabels([])
-        axl.set_yticklabels(np.linspace(vmin, vmax, 3),size = 'x-large')
-        axl.set_ylabel('$dva$\t\t', rotation=0, size='x-large')
-        axl.yaxis.set_label_coords(box.xmax+30,0.4)
-        axl.patch.set_alpha(0.5)
+        elif cbar == 'discrete':
+            colorbar_location= [0.8, 0.05, 0.1, 0.05]
+            cmaplist = [colmap(i) for i in range(colmap.N)]
+            bounds = np.linspace(vmin, vmax, cmap_steps + 1)  
+            bounds_label = np.linspace(vmin, vmax, 3)
+            norm = mpl.colors.BoundaryNorm(bounds, colmap.N)
 
-    elif cbar == 'discrete':
-        colorbar_location= [0.8, 0.05, 0.1, 0.05]
-        cmaplist = [colmap(i) for i in range(colmap.N)]
-        bounds = np.linspace(vmin, vmax, cmap_steps + 1)  
-        bounds_label = np.linspace(vmin, vmax, 3)
-        norm = mpl.colors.BoundaryNorm(bounds, colmap.N)
-        
-        cbar_axis = braindata_fig.add_axes(colorbar_location)
-        cb = mpl.colorbar.ColorbarBase(cbar_axis, cmap=colmap, norm=norm, ticks=bounds_label, boundaries=bounds,orientation='horizontal')
-        cb.set_label(cbar_label,size='x-large')
-        
-    elif cbar == '2D':
-        cbar_axis = braindata_fig.add_axes([0.8, 0.05, 0.15, 0.15])
-        base = cortex.utils.get_cmap(cmap)
-        cbar_axis.imshow(np.dstack((base.colors[...,0], base.colors[...,1], base.colors[...,2],base.colors[...,3])))
-        cbar_axis.set_xticks(np.linspace(0,255,3))
-        cbar_axis.set_yticks(np.linspace(0,255,3))
-        cbar_axis.set_xticklabels(np.linspace(vmin[0],vmax[0],3))
-        cbar_axis.set_yticklabels(np.linspace(vmax[1],vmin[1],3))
-        cbar_axis.set_xlabel(cbar_label[0], size='x-large')
-        cbar_axis.set_ylabel(cbar_label[1], size='x-large')
-        
+            cbar_axis = braindata_fig.add_axes(colorbar_location)
+            cb = mpl.colorbar.ColorbarBase(cbar_axis, cmap=colmap, norm=norm, ticks=bounds_label, boundaries=bounds,orientation='horizontal')
+            cb.set_label(cbar_label,size='x-large')
+
+        elif cbar == '2D':
+            cbar_axis = braindata_fig.add_axes([0.8, 0.05, 0.15, 0.15])
+            base = cortex.utils.get_cmap(cmap)
+            cbar_axis.imshow(np.dstack((base.colors[...,0], base.colors[...,1], base.colors[...,2],base.colors[...,3])))
+            cbar_axis.set_xticks(np.linspace(0,255,3))
+            cbar_axis.set_yticks(np.linspace(0,255,3))
+            cbar_axis.set_xticklabels(np.linspace(vmin[0],vmax[0],3))
+            cbar_axis.set_yticklabels(np.linspace(vmax[1],vmin[1],3))
+            cbar_axis.set_xlabel(cbar_label[0], size='x-large')
+            cbar_axis.set_ylabel(cbar_label[1], size='x-large')
+    else:
+        braindata_fig = []
+
     if add_roi == True:
         cortex.utils.add_roi(data=braindata, name=roi_name, open_inkscape=False, add_path=False, depth=depth, thick=thick, sampler=sampler, with_curvature=with_curvature,
                              with_colorbar=with_colorbar, with_borders=with_borders, curvature_brightness=curv_brightness, curvature_contrast=curv_contrast)
-
+        
     return braindata, braindata_fig
