6724086ed293c3c7d35913149ba1f4f2a8586348
mszinte <mszinte@invibe.nohost.me>
2025-01-09 17:10:55 +0100

    change to make it work with GPU

--- analysis_code/deepmreye/deepmreye_gpu_sbatch.py
+++ analysis_code/deepmreye/deepmreye_gpu_sbatch.py
@@ -1,6 +1,6 @@
 """
 -----------------------------------------------------------------------------------------
-deepmreye_sbatch.py
+deepmreye_gpu_sbatch.py
 -----------------------------------------------------------------------------------------
 Goal of the script:
 Run DeepMReye on mesocentre using job mode
@@ -8,8 +8,8 @@ Run DeepMReye on mesocentre using job mode
 Input(s):
 sys.argv[1]: main project directory
 sys.argv[2]: project name (correspond to directory)
+sys.argv[3]: gaze task
 sys.argv[3]: server nb of hour to request (e.g 10)
-sys.argv[4]: email account (e.g. martin.szinte@univ-amu.fr
 sys.argv[5]: data group (e.g. 327)
 sys.argv[6]: project name (e.g. b327)
 -----------------------------------------------------------------------------------------
@@ -25,10 +25,10 @@ python deepmreye_sbatch [main directory] [project name] [gaze_task] [hour proc.]
 -----------------------------------------------------------------------------------------
 Exemple:
 cd ~/projects/gaze_prf/analysis_code/deepmreye/
-python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeCenterFS 10 martin.szinte@univ-amu.fr 327 b327
-python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeCenter 10 martin.szinte@univ-amu.fr 327 b327
-python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeLeft 10 martin.szinte@univ-amu.fr 327 b327
-python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeRight 10 martin.szinte@univ-amu.fr 327 b327
+python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeCenterFS 1 327 b327
+python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeCenter 1 327 b327
+python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeLeft 1 327 b327
+python deepmreye_sbatch.py /scratch/mszinte/data gaze_prf GazeRight 1 327 b327
 -----------------------------------------------------------------------------------------
 Written by Martin Szinte (martin.szinte@gmail.com)
 -----------------------------------------------------------------------------------------
@@ -47,31 +47,29 @@ main_dir = sys.argv[1]
 project_dir = sys.argv[2]
 gaze_task = sys.argv[3]
 hour_proc = int(sys.argv[4])
-email = sys.argv[5]
-group = sys.argv[6]
-server_project = sys.argv[7]
+group = sys.argv[5]
+server_project = sys.argv[6]
 
 # Define cluster/server specific parameters
-nb_procs = 8
-cluster_name  = 'skylake'
-memory_val = 100
+nb_procs = 1
+cluster_name  = 'volta'
 log_dir = f"{main_dir}/{project_dir}/derivatives/deepmreye/log_outputs"
 
 # define SLURM cmd
 slurm_cmd = """\
 #!/bin/bash
 #SBATCH -p {cluster_name}
-#SBATCH --mail-user={email}
 #SBATCH -A {server_project}
 #SBATCH --nodes=1
-#SBATCH --mem={memory_val}gb
-#SBATCH --cpus-per-task={nb_procs}
+#SBATCH --gres=gpu:2
 #SBATCH --time={hour_proc}:00:00
-#SBATCH -e {log_dir}/deepmreye_%N_%j_%a.err
-#SBATCH -o {log_dir}/deepmreye_%N_%j_%a.out
-#SBATCH -J deepmreye_{gaze_task}
+#SBATCH -e {log_dir}/deepmreye_gpu_%N_%j_%a.err
+#SBATCH -o {log_dir}/deepmreye_gpu_%N_%j_%a.out
+#SBATCH -J deepmreye_gpu_{gaze_task}\n\n
+export PATH=$CONDA_PREFIX/bin:$PATH
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH\n
 """.format(server_project=server_project, nb_procs=nb_procs, hour_proc=hour_proc, gaze_task=gaze_task,
-		   memory_val=memory_val, log_dir=log_dir, email=email, cluster_name=cluster_name)
+		   log_dir=log_dir, email=email, cluster_name=cluster_name)
 
 # Define main cmd
 main_cmd = 'python deepmreye_analysis.py {} {} {} {}'.format(main_dir, project_dir, gaze_task, group)
